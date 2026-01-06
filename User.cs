b2165d2a910d9a21ee3256f2786bc8f2e8273cf1
andreeaciurciun <andreea.ciurciun@gmail.com>
2026-01-06 13:38:16 +0200

    modificare user

--- User.cs
+++ User.cs
@@ -1,26 +1,27 @@
+using System.IO;
+using System.Text.Json;
 namespace ProiectPoo;
 
-public enum UserRole
-{
-    Admin,
-    Customer
-}
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "Role")]
+[JsonDerivedType(typeof(Admin), "admin")]
+[JsonDerivedType(typeof(Customer), "customer")]
+
 
 abstract class User
 {
     public string Username{get;}
-    private string Password{get;}
+    public string Password{get; private set}
 
     protected User(string username, string password)
     {
         Username = username;
         Password = password;
     }
-
-    public bool CheckPassword(string input)
-    {
-        return Password == input;
-    }
-    
     public abstract UserRole Role { get; }
+    
+    public enum UserRole
+    {
+        Admin,
+        Customer
+    }
 }

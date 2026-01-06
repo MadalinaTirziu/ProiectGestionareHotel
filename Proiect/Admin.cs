94fad090fd20bf23059a5762cc565ccd00e16ba8
Madalina <madalina-nicoleta.tirziu@student.upt.ro>
2026-01-06 14:15:45 +0200

    responsabilitati admin

--- Proiect/Admin.cs
+++ Proiect/Admin.cs
@@ -1,12 +1,39 @@
+using Hotel.Room.Model;
+
 namespace Hotel.Admin.Model;
 using Hotel.User.Model;
+using Hotel.Room.Admin;
 
-public class Admin : User 
+public class Admin : User
 {
-    public Admin(string username, string password) : base(username, password)
+    private readonly AdministrareCamere _adminCamere;
+    public Admin(string username, string password, AdministrareCamere adminCamere) : base(username, password)
     {
-        
+        _adminCamere = adminCamere;
     }
-    
+
+    public void AdaugareCamera(int numar)
+    {
+        _adminCamere.AdaugaCamera(numar);
+    }
+
+    public void ModificareStatusCamera(int numar, StatusCamera status)
+    {
+        _adminCamere.SetareStatusCamera(numar, status);
+    }
+
+    public void EliminareCamera(int numar)
+    {
+        _adminCamere.StergeCamera(numar);
+    }
+
+    public void VizualizareCamere()
+    {
+        var camere =  _adminCamere.AfisareCamere();
+        foreach (var cam in camere)
+        {
+            Console.WriteLine($"Camera : {cam.Numar} | Status : {cam.StatusCamera}");
+        }
+    }
     public override UserRole Role => UserRole.Admin;
 }
